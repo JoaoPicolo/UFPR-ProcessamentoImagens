a7c7833aab8b5b5696562639ad6c82049e44598c
Joao <jpp18@inf.ufpr.br>
2022-06-26 22:58:01 -0300

    [main] Added cmake file

--- trab1/histograma.cpp
+++ trab1/histograma.cpp
@@ -1,10 +1,8 @@
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-
+#include <opencv2/core/core.hpp>
 #include <iostream>
 
 using namespace cv;
+using namespace std;
 
 int main() {
     return 0;
