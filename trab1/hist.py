b4b633736d08cef3b4cba52a1ecb149b11dba6ac
Joao <jpp18@inf.ufpr.br>
2022-06-29 23:50:44 -0300

    [main] Used confusion matrix for the metrics

--- trab1/hist.py
+++ trab1/hist.py
@@ -1,12 +1,13 @@
 import os
 import cv2
 
-BGR = [0, 1, 2]   # CV2 read BGR, not RGB
+BGR = [0, 1, 2]    # CV2 read BGR, not RGB
 
-CORRELATION = 0   # The higher, more accurate
-CHI_SQUARE = 1    # The lower, more accurate
-INTERSECTION = 2  # The higher, more accurate
-BHATTACHARYYA = 3 # The lower, more accurate
+CORRELATION = 0    # The higher, more accurate
+CHI_SQUARE = 1     # The lower, more accurate
+INTERSECTION = 2   # The higher, more accurate
+BHATTACHARYYA = 3  # The lower, more accurate
+
 
 def getDirImages(dirPath):
     images = []
@@ -18,7 +19,8 @@ def getDirImages(dirPath):
 
 
 def getNormalizedHist(image):
-    hist = cv2.calcHist([image], BGR, None, [256, 256, 256], [0, 256, 0, 256, 0, 256])
+    hist = cv2.calcHist([image], BGR, None, [256, 256, 256], [
+                        0, 256, 0, 256, 0, 256])
     hist = cv2.normalize(hist, hist)
 
     return hist
@@ -27,23 +29,40 @@ def getNormalizedHist(image):
 def getClass(name):
     base_name = name.split('.')[0]
     class_name = ''.join([char for char in base_name if not char.isdigit()])
-    
-    
+
     return class_name
 
 
-def calculateHistograms(dir_images):
+def getHistogramsAndClasses(dir_images):
     histograms = {}
+    classes = []
 
     for image_name in dir_images:
         img = cv2.imread(image_name)
         hist = getNormalizedHist(img)
+        image_class = getClass(image_name)
+
         histograms[image_name] = {
             "hist": hist,
-            "class": getClass(image_name)
+            "class": image_class
         }
 
-    return histograms
+        if image_class not in classes:
+            classes.append(image_class)
+
+    return histograms, classes
+
+
+def initConfusionMatrix(classes):
+    matrices = {}
+
+    for class_name in classes:
+        matrices[class_name] = {}
+
+        for option in classes:
+            matrices[class_name][option] = 0
+
+    return matrices
 
 
 def updatePrecision(candidate, new_class, new_value, higher_metric):
@@ -59,57 +78,85 @@ def updatePrecision(candidate, new_class, new_value, higher_metric):
         candidate["value"] = new_value
 
 
-def showResults(correlation, chi_square, intersection, bhattacharyya, samples):
-    print(f"Correlation accuracy: { correlation / samples }")
-    print(f"Chi-square accuracy: { chi_square / samples }")
-    print(f"Intersection accuracy: { intersection / samples }")
-    print(f"Bhattacharyya distance accuracy: { bhattacharyya / samples }")
-
-
-def calculatePrecisions(histograms):
-    correlation_hits = 0
-    chi_square_hits = 0
-    intersection_hits = 0
-    bhattacharyya_hits = 0
+def getConfusionMatrices(histograms, classes):
+    correlation_matrix = initConfusionMatrix(classes)
+    chi_square_matrix = initConfusionMatrix(classes)
+    intersection_matrix = initConfusionMatrix(classes)
+    bhattacharyya_matrix = initConfusionMatrix(classes)
 
     for test in histograms:
         test_hist = histograms[test]["hist"]
         test_class = histograms[test]["class"]
 
-        correlation_candidate = { "class": '', "value": 0 }
-        chi_square_candidate = { "class": '', "value": 0 }
-        intersection_candidate = { "class": '', "value": 0 }
-        bhattacharyya_candidate = { "class": '', "value": 0 }
+        correlation_candidate = {"class": '', "value": 0}
+        chi_square_candidate = {"class": '', "value": 0}
+        intersection_candidate = {"class": '', "value": 0}
+        bhattacharyya_candidate = {"class": '', "value": 0}
 
         for current in histograms:
-            current_hist = histograms[current]["hist"]
-            current_class = histograms[current]["class"]
-
             if test != current:
-                correlation = cv2.compareHist(test_hist, current_hist, CORRELATION)
-                updatePrecision(correlation_candidate, current_class, correlation, True)
+                current_hist = histograms[current]["hist"]
+                current_class = histograms[current]["class"]
 
-                chi_square = cv2.compareHist(test_hist, current_hist, CHI_SQUARE)
-                updatePrecision(chi_square_candidate, current_class, chi_square, False)
-                
-                intersection = cv2.compareHist(test_hist, current_hist, INTERSECTION)
-                updatePrecision(intersection_candidate, current_class, intersection, True)
+                correlation = cv2.compareHist(
+                    test_hist, current_hist, CORRELATION)
+                updatePrecision(correlation_candidate,
+                                current_class, correlation, True)
 
-                bhattacharyya = cv2.compareHist(test_hist, current_hist, BHATTACHARYYA)
-                updatePrecision(bhattacharyya_candidate, current_class, bhattacharyya, False)
+                chi_square = cv2.compareHist(
+                    test_hist, current_hist, CHI_SQUARE)
+                updatePrecision(chi_square_candidate,
+                                current_class, chi_square, False)
 
-        correlation_hits += (1 if (correlation_candidate["class"] == test_class) else 0)
-        chi_square_hits += (1 if (chi_square_candidate["class"] == test_class) else 0)
-        intersection_hits += (1 if (intersection_candidate["class"] == test_class) else 0)
-        bhattacharyya_hits += (1 if (bhattacharyya_candidate["class"] == test_class) else 0)
+                intersection = cv2.compareHist(
+                    test_hist, current_hist, INTERSECTION)
+                updatePrecision(intersection_candidate,
+                                current_class, intersection, True)
 
-    showResults(correlation_hits, chi_square_hits, intersection_hits, bhattacharyya_hits, len(histograms))
+                bhattacharyya = cv2.compareHist(
+                    test_hist, current_hist, BHATTACHARYYA)
+                updatePrecision(bhattacharyya_candidate,
+                                current_class, bhattacharyya, False)
+
+        correlation_matrix[test_class][correlation_candidate["class"]] += 1
+        chi_square_matrix[test_class][chi_square_candidate["class"]] += 1
+        intersection_matrix[test_class][intersection_candidate["class"]] += 1
+        bhattacharyya_matrix[test_class][bhattacharyya_candidate["class"]] += 1
+
+    return correlation_matrix, chi_square_matrix, intersection_matrix, bhattacharyya_matrix
+
+
+def printConfusionMatrix(method, matrix, samples):
+    total = 0
+    for item in matrix:
+        total += matrix[item][item]
+
+    print(f"{ method } accuracy: { total / samples } ({ total }/{ samples })")
+
+    print("  ", end=" ")
+    for item in matrix:
+        print(f"{ item }", end=" ")
+    print("\n")
+
+    for line in matrix:
+        print(f"{ line } ", end=" ")
+        for col in matrix:
+            print(f"{ matrix[line][col] }", end=" ")
+        print("\n")
 
 
 def main():
     dir_images = getDirImages('.')
-    histograms = calculateHistograms(dir_images)
-    calculatePrecisions(histograms)
+    histograms, classes = getHistogramsAndClasses(dir_images)
+    correlation, chi_square, intersection, bhattacharrya = getConfusionMatrices(
+        histograms, classes)
+
+    printConfusionMatrix("Correlation", correlation, len(histograms))
+    printConfusionMatrix("Chi-square", chi_square, len(histograms))
+    printConfusionMatrix("Intersection", intersection, len(histograms))
+    printConfusionMatrix("Bhattacharrya distance",
+                         bhattacharrya, len(histograms))
+
 
 if __name__ == "__main__":
     main()
