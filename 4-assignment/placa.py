577c4c837cbb37f44f74ef34eb34136f3d86b437
JOAO PEDRO PICOLO <jpp18@urquell>
2022-07-21 18:50:19 -0300

    [main] Added possible final code

--- 4-assignment/placa.py
+++ 4-assignment/placa.py
@@ -17,57 +17,6 @@ def showImage(fst_image, scd_image):
     cv2.imshow("Image", images)
     cv2.waitKey(0)
 
-
-def medianBlur(image):
-    processed = cv2.medianBlur(image, 3)
-    showImage(image, processed)
-
-
-def frequencyLowPass(image):
-    kernel = np.ones((35, 35), np.float32) / 25
-    processed = cv2.filter2D(image, -1, kernel)
-    processed = image - processed
-    showImage(image, processed)
-
-
-def gaussian(image):
-    processed = cv2.GaussianBlur(image, (7, 7), 0)
-    showImage(image, processed)
-
-
-def laplacian(image):
-    processed = cv2.Laplacian(processed, cv2.CV_64F, ksize=3)
-    showImage(image, processed)
-
-
-def sobel(image):
-    processedX = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=3)
-    processedY = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=3)
-    processed = processedX
-    showImage(image, processed)
-
-
-def canny(image):
-    processed = cv2.medianBlur(image, 3)
-    processed = cv2.Canny(processed, 10, 5)
-    showImage(image, processed)
-
-
-def fourier(image):
-    original = np.copy(image)
-    print(original is image)
-    f = np.fft.fft2(image)
-    fshift1 = np.fft.fftshift(f)
-
-    magnitude_specttrum = 20 * np.log(np.abs(fshift1))
-
-    f_ishift = np.fft.ifftshift(fshift1)
-    img_back = np.fft.fft2(f_ishift)
-    img_back = np.abs(img_back)
-
-    showImage(original, img_back)
-
-
 def main(args):
     in_path = args[0]
     # out_path = args[1]
@@ -75,7 +24,11 @@ def main(args):
 
     for image_name in images:
         image = cv2.imread(in_path + image_name, 0)
-        fourier(image)
+        clahe = cv2.createCLAHE(clipLimit=8.0, tileGridSize=(32, 32))
+        equalized = cv2.medianBlur(image, 3)
+        equalized = cv2.GaussianBlur(equalized, (5, 5), 0)
+        equalized = clahe.apply(equalized)
+        showImage(image, equalized)
 
 
 if __name__ == "__main__":
