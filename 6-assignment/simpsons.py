4ad5cf25ad3cf0272ee760be38db2fd8b32f304d
Joao <jpp18@inf.ufpr.br>
2022-09-08 21:25:03 -0300

    [main] Added histogram values

--- 6-assignment/simpsons.py
+++ 6-assignment/simpsons.py
@@ -1,9 +1,14 @@
 import os
 
 import cv2
+import numpy as np
+
 from sklearn.neighbors import KNeighborsClassifier
+
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
+
+# TODO: Falar no relatorio que nao usou concavidade pq nao faz sentido
 
 
 CLASS_MAP = {
@@ -40,6 +45,7 @@ def readImages(dir_path):
     return images
 
 
+# TODO: Testar binarizado
 def applyHuMoments(images):
     x_values, y_values = [], []
 
@@ -52,12 +58,65 @@ def applyHuMoments(images):
     return x_values, y_values
 
 
+# TODO: Fazer rodar na KNN
+# TODO: Testar binarizado
+def applyContours(images):
+    x_values, y_values = [], []
+
+    for image_data in images:
+        image = cv2.imread(image_data["path"], 0)
+        contours, _ = cv2.findContours(
+            image, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+        x_values.append(contours)
+        y_values.append(image_data["class"])
+
+    return x_values, y_values
+
+
+# TODO: Testar binarizado
+# TODO: Testar com cor
+# TODO: Testar com canal
+# TODO: Testar com histograma normalizado
+def applyHistogram(images):
+    x_values, y_values = [], []
+
+    for image_data in images:
+        image = cv2.imread(image_data["path"], 0)
+        hist = cv2.calcHist([image], [0], None, [256], [0, 256]).flatten()
+        print(hist.shape)
+        x_values.append(hist)
+        y_values.append(image_data["class"])
+
+    return x_values, y_values
+
+
+# TODO: Mesmo de cima
+# Default is x-axis
+def applyProjectedHistogram(images, axis=0):
+    x_values, y_values = [], []
+
+    for image_data in images:
+        image = cv2.imread(image_data["path"], 0)
+        image = cv2.resize(image, (256, 256), interpolation=cv2.INTER_AREA)
+        hist = np.sum(image, axis=axis).astype(int).flatten()
+        x_values.append(hist)
+        y_values.append(image_data["class"])
+
+    return x_values, y_values
+
+
 def getSets(dir_path, method):
     images = readImages(dir_path)
     x_values, y_values = [], []
 
-    if method == 'huMoments':
+    if method == 'HuMoments':
         x_values, y_values = applyHuMoments(images)
+    elif method == 'Contours':
+        x_values, y_values = applyContours(images)
+    elif method == 'Histogram':
+        x_values, y_values = applyHistogram(images)
+    elif method == 'ProjectedHistogram':
+        x_values, y_values = applyProjectedHistogram(images, 1)
     else:
         print("Please provide a valid method")
         exit(0)
@@ -81,8 +140,8 @@ def knnClassify(X_train, y_train, X_test, y_test, neighbors, metric):
 
 
 def main():
-    X_train, y_train = getSets('./train', 'huMoments')
-    X_test, y_test = getSets('./validation', 'huMoments')
+    X_train, y_train = getSets('./train', 'ProjectedHistogram')
+    X_test, y_test = getSets('./validation', 'ProjectedHistogram')
 
     knnClassify(X_train, y_train, X_test, y_test, 1, 'euclidean')
 
